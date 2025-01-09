e2e84100a65042e33920b6ef87248cc5920cb33e
Victor S Mendes <victormendes1993@live.com>
2025-01-09 09:34:39 -0300

    update to test

--- tests/user.test.js
+++ tests/user.test.js
@@ -100,6 +100,8 @@ test('Should delete account for user', async () => {
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user).toBeNull()
 })
 
 test('Should not delete account for unauthenticated user', async () => {
